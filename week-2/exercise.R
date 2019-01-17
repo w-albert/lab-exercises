9eb5b4b2a6034bae7e8389ef84539a8848e340a7
kishore <viprankishore@gmail.com>
2019-01-16 20:44:59 -0800

    solutions

--- week-2/exercise.R
+++ week-2/exercise.R
@@ -9,7 +9,7 @@ my_vector <- c(my_num, initials)
 vector_sum <- sum(my_vector)
 
 # Describe why this doesn't work: 
-# 
+# "sum()" only works with numbers and initials is letters
 
 install.packages("stringr")
 
@@ -18,35 +18,63 @@ my_line <- "Hey, hey, this is the library"
 print(str_length(my_line))
 
 # Describe why this doesn't work: 
-# 
+# "library()" hasn't been used on the stringr package
 
 said_the_famous <- paste(my_line, " - ", initial)
 
 # Describe why this doesn't work: 
-# 
+# `initial`` is not a variable stored in the environment
 
 
 ## Part 2 - Vector and function practice
 
 # Make a vector and use typeof to check what type R considers it to be
+typeof(c("dogs", "cats", "ferrets???"))  # "character"
+
+typeof(c(1, 4, 6))  # "double"
+
+typeof(c(1, "dogs"))  # "character"
 
 # Write a function `compare_length` that takes in 2 vectors, and returns the sentence:
 # "The difference in lengths is N"
+compare_length <- function(v1, v2) {
+  diff <- abs(length(v1) - length(v2))
+  diff_string <- paste('The difference in lengths is', diff)
+  return(diff_string)
+}  
 
-
-# Pass two vectors of different length to your `compare_length` function
-
+# Pass two vectors of different length to your `CompareLength` function
+compare_length(c(1,2,3), 1:100)
 
 # Write a function `describe_difference` that will return one of the following statements:
 # "Your first vector is longer by N elements"
 # "Your second vector is longer by N elements"
+describe_difference <- function(a,b) {
+  diff <- length(a) - length(b)
+  if(diff > 0) {
+    sentence <- paste('Your first vector is longer by', diff, 'elements')
+  } else {
+    sentence <- paste('Your second vector is longer by', -diff, 'elements')
+  }
+  return(sentence)
+}
 
-# Pass two vectors to your `describe_difference` function
+# Pass two vectors to your `DescribeDifference` function
+describe_difference(1:15, 9:17)
 
 
 # Write a function `combine_vectors` that takes in 3 vectors and combines them into one
+combine_vectors <- function(v1, v2, v3) {
+  return(c(v1, v2, v3))
+}
 
 # Send 3 vectors to your function to test it.
+combine_vectors(c("Birds ", "of ", "a "), c("feather "), c("flock ", "together"))
 
 # Write a function `caps_time` that takes in a vector of names of courses you're taking, ex "Informatics 201" and removes all capital letters
 
+caps_time <- function(courses) {
+  return(gsub("[A-Z]","", courses))
+}
+
+caps_time(c("Informatics", "Bioethics", "Artifical Intelligence"))
--- week-3/lab3-exercise.R
+++ week-3/lab3-exercise.R
@@ -2,23 +2,63 @@ data(iris)
 View(iris)
 
 #Get all rows of Species ‘versicolor’ in a new data frame. Call this data frame: ‘iris.vers’
+iris.vers = iris[iris$Species == "versicolor",]
+View(iris.vers)
 
 #Get a vector called ‘sepal.dif’ with the difference between ‘Sepal.Length’ and ‘Sepal.Width’ of ‘versicolor’ plants.
+sepal.dif <- iris.vers$Sepal.Length - iris.vers$Sepal.Width
 
 #Add ‘iris.vers’ with the new column ‘sepal.dif’.
+iris.vers$sepal.dif <- sepal.dif
 
 #filter for all data of Species ‘virginica’ with a ‘Sepal.Width’ of greater than 3.5.
+iris[iris$Species == 'virginica' & iris$Sepal.Width >3.5,]
 
 #get a data frame of  all data of Species ‘setosa’ with a ‘Sepal.Width’ of greater than 3.5, but without the last column Species in the data frame?
+iris[iris$Species == "setosa" & iris$Sepal.Width > 3.5, 1:4]
 
 #Write a function that takes in a species name as an argument and returns the average Sepal Width of that species.
+f <- function(sp_name,arg){
+  tmpdf <- iris[iris$Species==sp_name,]
+  print(paste("Mean of Sepal Width for ",sp_name,": ",mean(tmpdf[,names(tmpdf)==arg])))
+}
+f("setosa","Sepal.Width")
 
 #What is the average Sepal.Length for 'versicolor' species having a Petal.Length of 4.2 or higher?
+mean(iris[iris$Species == 'versicolor' & iris$Petal.Length>=4.2,"Sepal.Length"])
 
 #BONUS VIZ Q
 # Visualize species, Sepal.Length and Sepal.Width in a single plot. DO YOU SEE CLUSTERS?
 
+plot(iris$Sepal.Length,iris$Sepal.Width,col = iris$Species,pch = 16)
+legend("topright",legend=paste(unique(iris$Species)),pch=rep(c(16,18),each=4),bty="n",ncol=2,cex=0.7,pt.cex=0.7,col = c("black","red","green"))
 
 
 #ULTRA BONUS - PERFORM PRINCIPAL COMPONENT ANALYSIS AND PLOT CLUSTERS
 
+install.packages('ggfortify')
+install.packages('ggalt')
+
+
+library(ggalt)
+library(ggfortify)
+
+df <- iris[c(1, 2, 3, 4)]
+pca_mod <- prcomp(df)  # compute principal components
+
+# Data frame of principal components ----------------------
+df_pc <- data.frame(pca_mod$x, Species=iris$Species)  # dataframe of principal components
+df_pc_vir <- df_pc[df_pc$Species == "virginica", ]  # df for 'virginica'
+df_pc_set <- df_pc[df_pc$Species == "setosa", ]  # df for 'setosa'
+df_pc_ver <- df_pc[df_pc$Species == "versicolor", ]  # df for 'versicolor'
+
+ggplot(df_pc, aes(PC1, PC2, col=Species)) + 
+  geom_point(aes(shape=Species), size=2) +   # draw points
+  labs(title="Iris Clustering", 
+       subtitle="With principal components PC1 and PC2 as X and Y axis",
+       caption="Source: Iris") + 
+  coord_cartesian(xlim = 1.2 * c(min(df_pc$PC1), max(df_pc$PC1)), 
+                  ylim = 1.2 * c(min(df_pc$PC2), max(df_pc$PC2))) +   # change axis limits
+  geom_encircle(data = df_pc_vir, aes(x=PC1, y=PC2)) +   # draw circles
+  geom_encircle(data = df_pc_set, aes(x=PC1, y=PC2)) + 
+  geom_encircle(data = df_pc_ver, aes(x=PC1, y=PC2))
