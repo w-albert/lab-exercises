479039a9671ffe096e6c6525ec7b346915340a54
w-albert <al.wang.me@gmail.com>
2019-04-22 11:08:22 -0700

    Does something with petals??

--- week-3/lab3-exercise.R
+++ week-3/lab3-exercise.R
@@ -1,24 +1,37 @@
 data(iris)
 View(iris)
 
-#Get all rows of Species ‘versicolor’ in a new data frame. Call this data frame: ‘iris.vers’
+#Get all rows of Species 'versicolor' in a new data frame. Call this data frame:
+#'iris.vers'
+is_versi <- iris$Species == "versicolor"
+iris.vers <- data.frame(iris[is_versi, ], stringsAsFactors = FALSE)
 
-#Get a vector called ‘sepal.dif’ with the difference between ‘Sepal.Length’ and ‘Sepal.Width’ of ‘versicolor’ plants.
+#Get a vector called 'sepal.dif' with the difference between 'Sepal.Length' and 
+#'Sepal.Width' of 'versicolor' plants.
+sepal.dif <- iris.vers$Sepal.Length - iris.vers$Sepal.Width
 
-#Add ‘iris.vers’ with the new column ‘sepal.dif’.
+#Add 'iris.vers' with the new column 'sepal.dif'.
+iris.vers$sepal.dif <- sepal.dif
 
-#filter for all data of Species ‘virginica’ with a ‘Sepal.Width’ of greater than 3.5.
+#filter for all data of Species 'virginica' with a 'Sepal.Width' of greater 
+#than 3.5.
+iris[iris$Species == "virginica" & iris$Sepal.Width > 3.5, ]
 
-#get a data frame of  all data of Species ‘setosa’ with a ‘Sepal.Width’ of greater than 3.5, but without the last column Species in the data frame?
+#get a data frame of  all data of Species 'setosa' with a 'Sepal.Width' of 
+#greater than 3.5, but without the last column Species in the data frame?
+setosa_only <- iris[iris$Species == "setosa", ]
+wide_setosa <- setosa_only[setosa_only$Sepal.Width > 3.5, -5]
 
-#Write a function that takes in a species name as an argument and returns the average Sepal Width of that species.
+#Write a function that takes in a species name as an argument and returns the 
+#average Sepal Width of that species.
 
-#What is the average Sepal.Length for 'versicolor' species having a Petal.Length of 4.2 or higher?
+#What is the average Sepal.Length for 'versicolor' species having a Petal.Length
+#of 4.2 or higher?
 
 #BONUS VIZ Q
-# Visualize species, Sepal.Length and Sepal.Width in a single plot. DO YOU SEE CLUSTERS?
+# Visualize species, Sepal.Length and Sepal.Width in a single plot. 
+#DO YOU SEE CLUSTERS?
 
 
 
 #ULTRA BONUS - PERFORM PRINCIPAL COMPONENT ANALYSIS AND PLOT CLUSTERS
-
